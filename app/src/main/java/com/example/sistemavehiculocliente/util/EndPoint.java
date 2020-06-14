cb07c4bef087905076c85e837bb421add390ed9a
DanielAristy <danielariaty@gmail.com>
2020-06-14 14:53:38 -0500

    Creando Uris de servicios que vamos a consumir desde intelliJ

---
+++ app/src/main/java/com/example/sistemavehiculocliente/util/EndPoint.java
@@ -0,0 +1,13 @@
+package com.example.sistemavehiculocliente.util;
+
+public class EndPoint {
+
+    public static final long CONNECTION_TIMEOUT_RETROFIT = 40;
+    public static final String URL_BASE = "http://http://192.168.1.55:8080/";
+
+    public static final String INSERTAR_VEHICULO = "control-vehiculo/vehiculo";
+//    public static final String ACTUALIZAR_VEHICULO = "control-vehiculo/{placa}/vehiculo";
+//    public static final String ELIMINAR_VEHICULO = "control-vehiculo/{placa}/vehiculo";
+    public static final String LISTAR = "control-vehiculo/vehiculos";
+
+}
