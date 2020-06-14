147714978907d00b1e2d465758eb12aef57c8c16
DanielAristy <danielariaty@gmail.com>
2020-06-14 14:48:35 -0500

    Creando modelo con sus respecivos atributos

---
+++ app/src/main/java/com/example/sistemavehiculocliente/model/Vehiculo.java
@@ -0,0 +1,12 @@
+package com.example.sistemavehiculocliente.model;
+
+import lombok.Data;
+
+@Data
+public class Vehiculo {
+
+    private String color;
+    private String marca;
+    private String modelo;
+    private String placa;
+}
